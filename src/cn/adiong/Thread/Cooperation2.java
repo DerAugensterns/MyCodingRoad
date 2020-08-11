4761b14383c1306a5b9c47d960a1caaf779672a9
DerAugensterns <1289720889@qq.com>
2020-08-11 16:43:21 +0800

    java入门学习

--- src/cn/adiong/Thread/Cooperation2.java
+++ src/cn/adiong/Thread/Cooperation2.java
@@ -2,14 +2,10 @@ package cn.adiong.Thread;
 
 /**
  * @Author: 阿威
- * @Date: 2020/8/11 15:54
+ * @Date: 2020/8/11 16:42
  */
 public class Cooperation2 {
     public static void main(String[] args) {
-        System.out.println("fuck ");
-        System.out.println("fuck ");
-        System.out.println("fuck ");
-        System.out.println("fuck ");
-        System.out.println("fuck ");
+        System.out.println("fuxxxxxxxxxx");
     }
 }
